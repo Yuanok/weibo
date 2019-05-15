88eaced0893b7f2f004cc27babed5540a39da41c
Yuanok2 <382547456@qq.com>
2019-05-15 11:46:05 +0800

    增加csrf验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -14,4 +14,13 @@ class UsersController extends Controller
     public function show(User $user){
         return view('users.show',['user'=>$user]);
     }
+
+    public function store(Request $request){
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+        return;
+    }
 }
